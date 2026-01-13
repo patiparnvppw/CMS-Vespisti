e8a7f46f9238108e05094d26b45608b9060bcfcd
patiparnnn <128585462+patiparnnn@users.noreply.github.com>
2026-01-13 13:50:42 +0700

    Update tests

---
+++ tests/customer.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Customer', () => {
+    test('should show login page', async ({ page }) => {
+        await page.goto('/signin');
+
+        await expect(page).toHaveURL(/\/signin/);
+        await expect(
+            page.getByRole('button', { name: 'Sign in' })
+        ).toBeVisible();
+    });
+
+});
